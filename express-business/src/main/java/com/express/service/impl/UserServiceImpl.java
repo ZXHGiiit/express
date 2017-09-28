dc6352e30981af029783f4175a6178fc22c85fc8
周星航 <xinghang.zhou@renren-inc.com>
2017-09-28 19:38:45 +0800

    实现单点登录

--- express-business/src/main/java/com/express/service/impl/UserServiceImpl.java
+++ express-business/src/main/java/com/express/service/impl/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.express.service.impl;
 
+import com.google.common.base.Strings;
+
 import com.express.commons.service.RedisService;
+import com.express.commons.util.CookieUtils;
+import com.express.commons.util.JacksonUtils;
 import com.express.dao.UserDao;
 import com.express.domain.User;
 import com.express.service.UserService;
@@ -9,6 +13,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,20 +35,55 @@ public class UserServiceImpl implements UserService {
   @Autowired
   private RedisService redisService;
 
+  /**
+   * 若登录成功，将用户信息存入redis，以uuid作为key
+   * 将token作为key，uuid作为value建立一个cookie，
+   */
   @Override
   public String login(String name, String password, HttpServletRequest req, HttpServletResponse
     resp) {
-    return null;
+    if (Strings.isNullOrEmpty(name) || Strings.isNullOrEmpty(password)) {
+      LOG.error("UserServiceImpl.login.name or password is NULL");
+      return "";
+    }
+    User user = userDao.selectByUAP(name, password);
+    if (user == null) {
+      LOG.info("UserServiceImpl.login incorrect");
+      return "";
+    }
+    LOG.info("UserServiceImpl.login success user: " + user.toString());
+    //生成uuid，一个uuid对应一个用户
+    String uuid = UUID.randomUUID().toString();
+    //将用户信息存入redis，uuid作为key
+    redisService.set("express", uuid, JacksonUtils.toJson(user));
+    redisService.expire("express", uuid, 30 * 60);
+    //将token最为key，uuid作为value，建立cookie。
+    CookieUtils.setCookie(req, resp, TT_TOKEN, uuid, 30 * 60);
+    return uuid;
   }
 
   @Override
   public String selByToken(String token) {
-    return null;
+    if (Strings.isNullOrEmpty(token)) {
+      LOG.error("UserServiceImpl.selByToken.token is null");
+      return "";
+    }
+    return redisService.get("express", token);
   }
 
+  /**
+   * 删除保存了用户uuid的cookie和redis缓存
+   * 通过req和token来获得该cookie
+   */
   @Override
   public void logout(HttpServletRequest req, HttpServletResponse resp) {
-
+    String cookieValue = CookieUtils.getCookieValue(req, TT_TOKEN);
+    if (Strings.isNullOrEmpty(cookieValue)) {
+      LOG.info("UserServiceImpl.logout.getCookie is failed");
+    } else {
+      CookieUtils.deleteCookie(req, resp, TT_TOKEN);
+      redisService.del("express", cookieValue);
+    }
   }
 
   @Override
--- express-web/src/main/java/com/express/controller/login/LoginController.java
+++ express-web/src/main/java/com/express/controller/login/LoginController.java
@@ -1,8 +1,22 @@
 package com.express.controller.login;
 
+import com.google.common.base.Strings;
+
+import com.express.commons.constant.ErrorCodeEnum;
+import com.express.commons.util.RetJacksonUtil;
+import com.express.commons.vo.ExpressResult;
+import com.express.service.UserService;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by xinghang on 17/9/28.
@@ -12,5 +26,35 @@ import org.springframework.stereotype.Controller;
 public class LoginController {
   private Log LOG = LogFactory.getLog(LoginController.class);
 
+  @Autowired
+  private UserService userService;
+
+  @RequestMapping("/login")
+  @ResponseBody
+  public String login(String name, String password, HttpServletRequest req, HttpServletResponse
+    resp) {
+    String token = userService.login(name, password, req, resp);
+    if (token == null) {
+      LOG.error("UserController.login.ERROR.token is null");
+      return RetJacksonUtil.resultWithFailed(ErrorCodeEnum.NO_TOKEN);
+    } else {
+      return RetJacksonUtil.resultOk();
+    }
+  }
+
+
+  @RequestMapping("/token/{token}")
+  @ResponseBody
+  public Object token(@PathVariable String token, String callback) {
+    if (Strings.isNullOrEmpty(token)) {
+      LOG.error("UserController.token.token is null");
+      return "";
+    }
+    String result = userService.selByToken(token);
+    LOG.info("LoginController.token.callback.result = " + result);
+    ExpressResult expressResult = new ExpressResult();
+
+
+  }
 
 }
