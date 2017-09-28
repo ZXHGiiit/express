7b887d1c36dd1b68d2644a7d778bd3906b2c4764
周星航 <xinghang.zhou@renren-inc.com>
2017-09-28 19:36:17 +0800

    添加字符常量

--- express-commons/src/main/java/com/express/commons/constant/StringConstants.java
+++ express-commons/src/main/java/com/express/commons/constant/StringConstants.java
@@ -4,4 +4,17 @@ package com.express.commons.constant;
  * Created by xinghang on 17/9/28.
  */
 public class StringConstants {
+  public static final String AUTHER_NAME = "zhouxinghang";
+
+  public static final String RESULT_CODE = "code";
+
+  public static final String RESULT_MSG = "msg";
+
+  public static final String RESULT_STATUS = "status";
+
+  public static final String RESULT_DATA = "data";
+
+  public static final int VALUE_NOT_HASMORE = 0;
+
+  public static final int VALUE_HASMORE = 1;
 }
