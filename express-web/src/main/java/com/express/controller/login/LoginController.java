648dff0cbedcc52ef414aa4f261c422e13765bcf
周星航 <xinghang.zhou@renren-inc.com>
2017-09-29 15:56:18 +0800

    LoginController

--- express-web/src/main/java/com/express/controller/login/LoginController.java
+++ express-web/src/main/java/com/express/controller/login/LoginController.java
@@ -10,6 +10,7 @@ import com.express.service.UserService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +36,7 @@ public class LoginController {
     resp) {
     String token = userService.login(name, password, req, resp);
     if (token == null) {
-      LOG.error("UserController.login.ERROR.token is null");
+      LOG.error("LoginController.login.ERROR.token is null");
       return RetJacksonUtil.resultWithFailed(ErrorCodeEnum.NO_TOKEN);
     } else {
       return RetJacksonUtil.resultOk();
@@ -47,14 +48,53 @@ public class LoginController {
   @ResponseBody
   public Object token(@PathVariable String token, String callback) {
     if (Strings.isNullOrEmpty(token)) {
-      LOG.error("UserController.token.token is null");
+      LOG.error("LoginController.token.token is null");
       return "";
     }
     String result = userService.selByToken(token);
     LOG.info("LoginController.token.callback.result = " + result);
     ExpressResult expressResult = new ExpressResult();
+    if (!Strings.isNullOrEmpty(result)) {
+      expressResult.setStatus(200);
+      expressResult.setMsg("OK");
+      expressResult.setData(result);
+    } else {
+      expressResult.setStatus(500);
+      expressResult.setMsg("ERROR");
+    }
+    if (!Strings.isNullOrEmpty(callback)) {
+      LOG.info("LoginController.token.callback = " + callback);
+      MappingJacksonValue mjv = new MappingJacksonValue(expressResult);
+      mjv.setJsonpFunction(callback);
+      return mjv;
+    } else {
+      LOG.info("LoginController.token.callback is null");
+      return expressResult;
+    }
 
+  }
 
+  @RequestMapping("/logout")
+  @ResponseBody
+  public Object logout(HttpServletRequest req, HttpServletResponse resp, String callback) {
+    ExpressResult expressResult = new ExpressResult();
+    try {
+      userService.logout(req, resp);
+      expressResult.setStatus(200);
+      expressResult.setMsg("OK");
+      expressResult.setData("");
+    } catch (Exception e) {
+      LOG.error("UserController.logout.ERROR", e);
+    }
+    if (!Strings.isNullOrEmpty(callback)) {
+      LOG.info("LoginController.logout.callback = " + callback);
+      MappingJacksonValue mjv = new MappingJacksonValue(expressResult);
+      mjv.setJsonpFunction(callback);
+      return mjv;
+    } else {
+      LOG.info("LoginController.logout.callback is null");
+      return expressResult;
+    }
   }
 
 }
