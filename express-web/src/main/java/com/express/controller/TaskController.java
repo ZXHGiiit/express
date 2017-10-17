0da3c3b2cb68e2a9233a3fe9fcb62ce71a7526e8
xinghang.zhou <13633843273@163.com>
2017-10-17 20:46:35 +0800

    添加任务发布Controller

---
+++ express-web/src/main/java/com/express/controller/TaskController.java
@@ -0,0 +1,49 @@
+package com.express.controller;
+
+import com.express.commons.util.RetJacksonUtil;
+import com.express.interceptor.HostHolder;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 用户任务Controller
+ */
+
+@Controller("/task")
+public class TaskController {
+    private static final Log LOG = LogFactory.getLog(TaskController.class);
+
+    @Autowired
+    private HostHolder holder;
+    /**
+     * 发布任务
+     * @param startAdd
+     * @param endAdd
+     * @return
+     */
+    public String add(String startAdd, String endAdd) {
+        return RetJacksonUtil.resultOk();
+    }
+
+    /**
+     * 删除任务
+     * 可有多个任务，通过id来删除
+     * @return
+     */
+    public String delete(int taskId) {
+        return RetJacksonUtil.resultOk();
+    }
+
+    /**
+     * 修改任务
+     * @param startAdd
+     * @param endAdd
+     * @return
+     */
+    public String update(int taskId, String startAdd, String endAdd) {
+        return RetJacksonUtil.resultOk();
+    }
+}
