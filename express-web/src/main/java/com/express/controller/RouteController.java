8c0f3ac6ebe02d92df328127ebe71a6f09662ef6
xinghang.zhou <13633843273@163.com>
2017-10-17 20:47:37 +0800

    添加用户行程发布Controller

---
+++ express-web/src/main/java/com/express/controller/RouteController.java
@@ -0,0 +1,50 @@
+package com.express.controller;
+
+import com.express.commons.util.RetJacksonUtil;
+import com.express.interceptor.HostHolder;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 用户行程Controller
+ */
+
+@Controller("/route")
+public class RouteController {
+    private Log LOG = LogFactory.getLog(RouteController.class);
+
+    @Autowired
+    private HostHolder holder;
+
+    /**
+     * 发布行程
+     * @param startAdd
+     * @param endAdd
+     * @return
+     */
+    public String add(String startAdd, String endAdd) {
+        return RetJacksonUtil.resultOk();
+    }
+
+    /**
+     * 删除行程
+     * @return
+     */
+    public String delete() {
+        return RetJacksonUtil.resultOk();
+    }
+
+    /**
+     * 修改行程
+     * @param startAdd
+     * @param endAdd
+     * @return
+     */
+    public String update(String startAdd, String endAdd) {
+        return RetJacksonUtil.resultOk();
+    }
+
+}
