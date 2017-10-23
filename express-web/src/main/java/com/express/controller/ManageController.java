607d753465477e925b3249282f818d819a978dee
xinghang.zhou <13633843273@163.com>
2017-10-23 19:32:42 +0800

    添加ManageController

--- express-web/src/main/java/com/express/controller/ManageController.java
+++ express-web/src/main/java/com/express/controller/ManageController.java
@@ -1,4 +1,18 @@
 package com.express.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class ManageController {
+    @RequestMapping("/")
+    public String welcome() {
+        return "index";
+    }
+
+    @RequestMapping("{page}")
+    public String showPage(@PathVariable String page) {
+        return page;
+    }
 }
