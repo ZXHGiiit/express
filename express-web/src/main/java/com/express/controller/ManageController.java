350a04d4d1d6431db4334bf32b3b6feec28406b3
xinghang.zhou <13633843273@163.com>
2017-10-23 23:40:01 +0800

    添加log

--- express-web/src/main/java/com/express/controller/ManageController.java
+++ express-web/src/main/java/com/express/controller/ManageController.java
@@ -1,11 +1,15 @@
 package com.express.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ManageController {
+    private static final Log LOG = LogFactory.getLog(ManageController.class);
+
     @RequestMapping("/")
     public String welcome() {
         return "index";
@@ -13,6 +17,7 @@ public class ManageController {
 
     @RequestMapping("{page}")
     public String showPage(@PathVariable String page) {
+        LOG.info("we are arriving at " + page);
         return page;
     }
 }
